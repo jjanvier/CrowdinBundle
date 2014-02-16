870bb1743af1b15deedb5c73f37f788e5f7a0da5
Julien Janvier <j.janvier@gmail.com>
2014-02-16 21:33:29 +0100

    Update JjanvierCrowdinExtension.php

--- DependencyInjection/JjanvierCrowdinExtension.php
+++ DependencyInjection/JjanvierCrowdinExtension.php
@@ -7,7 +7,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- */s
+ */
 
 namespace Jjanvier\Bundle\CrowdinBundle\DependencyInjection;
 
